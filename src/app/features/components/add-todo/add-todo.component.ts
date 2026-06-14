642932e220ce5a3ced43a705edd81103b14377b2
Shubham Dalavi <shubhamdalavi20@gmail.com>
2026-06-15 00:24:30 +0530

    Added rhome, todo list, add todo compoent with router outlet and css

---
+++ src/app/features/components/add-todo/add-todo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  imports: [],
+  templateUrl: './add-todo.component.html',
+  styleUrl: './add-todo.component.scss'
+})
+export class AddTodoComponent {
+
+}
